a109ede555f352dfccc0fcdd681b9941bfa62b06
Thomas de Mareuil <tdemareuil@hotmail.fr>
2020-10-29 00:27:08 +0100

    Update __init__.py

---
+++ totalsat/__init__.py
@@ -0,0 +1,15 @@
+
+"""
+This package is an augmented version of some modules from the `coastsat` package by Kilian Vos.
+The goal was to write high-level, easy-to-use functions leveraging existing libraries, in order 
+to simplify how we search for, download and manipulate satellite imagery within iPython notebooks. 
+Tools available in this package include image preprocessing (remote sensing indices), labeling (to 
+prepare data for ML / DL model training), slicing and other satellite imagery-related utilities.
+"""
+__author__ = "Thomas de Mareuil, "
+__credits__ = ["Kilian Vos (coastsat)", "Frank Warmerdam (gdal_merge)", "Sam Dobson (image_slicer)"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "xx"
+__email__ = "xx"
+__status__ = "Prototype"
